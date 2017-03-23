297ae5c915274ea7875279152c4bbcebc7cb1810
Pierre Gauthier <pirgauthier@gmail.com>
2017-03-23 18:18:08 +0100

    Few model methods

--- app/models/intercommunality.rb
+++ app/models/intercommunality.rb
@@ -2,6 +2,14 @@ class Intercommunality < ApplicationRecord
   has_many :communes
   
   validates :name, presence: true
-  validates :siren, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A\d{9}\Z/}
-  validates :form, inclusion: {in: %w(ca cu cc met)}
+  validates :siren, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\b\d{9}\b/}
+  validates :form, acceptance: { accept: ['ca', 'cu', 'cc', 'met'] }
+  
+  attribute :slug, :string
+  
+  after_save { self.slug = self.name.parameterize if self.slug==nil }
+  
+  def communes_hash
+    self.communes.map{ |c| [c.code_insee,c.name] }.to_h
+  end
 end
